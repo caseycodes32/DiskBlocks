39ea1a0af14d084be9a329859c546b0ea7176ea1
Kyle Meyer <vegas.kylem@gmail.com>
2025-02-28 05:20:43 -0800

    Update build script
    - Updated build script to modify output directory and filename

--- build_mingw.bat
+++ build_mingw.bat
@@ -1,6 +1,6 @@
 @REM Build for MINGW64 or 32 from MSYS2.
-@set OUT_DIR=Debug
-@set OUT_EXE=example_win32_opengl3
+@set OUT_DIR=Output
+@set OUT_EXE=DiskBlocks
 @set INCLUDES=-I imgui/ -I imgui/backends
 @set SOURCES=main.cpp imgui/backends/imgui_impl_opengl3.cpp imgui/backends/imgui_impl_win32.cpp imgui/imgui*.cpp
 @set LIBS=-lopengl32 -lgdi32 -ldwmapi
