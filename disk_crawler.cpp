6b82493f78a1a1308d5ed31997a3ca8719d537b3
Kyle Meyer <vegas.kylem@gmail.com>
2025-03-07 21:28:40 -0800

    Implement File Tree
    - Adds data type DiskElement that serves as a file structure tree, which contains each file's:
      name
      directory status
      size
      children (if directory)
    
    - Adds function GetFileSize to acquire file size given a path
    
    - Adds function PopulateTree which populates a given tree data structure, given a path
    
    - Adds a couple small helper functions

--- disk_crawler.cpp
+++ disk_crawler.cpp
@@ -1,6 +1,5 @@
 #include "disk_crawler.h"
 
-
 std::vector<std::string> ListDrives()
 {
     std::vector<std::string> vDriveStrings;
@@ -31,11 +30,88 @@ std::vector<std::string> ListSubDirectories(std::string path)
             direntIterator = readdir(dirCurrentDirectory);
             if (direntIterator != nullptr)
             {
-                vDirectoryStrings.push_back(direntIterator->d_name);
+                if (PathIsDirectory(path + "/" + direntIterator->d_name))
+                    vDirectoryStrings.push_back(path + "/" + direntIterator->d_name);
             }
             else break;
         }
         closedir (dirCurrentDirectory);
     }
     return vDirectoryStrings;
+}
+
+std::vector<std::string> ListElementsInDirectory(std::string path)
+{
+    std::vector<std::string> vElementStrings;
+    
+    DIR* dirCurrentDirectory = opendir(path.c_str());
+    struct dirent* direntIterator;
+    if (dirCurrentDirectory != nullptr)
+    {
+        while (true)
+        {
+            direntIterator = readdir(dirCurrentDirectory);
+            if (direntIterator != nullptr)
+            {
+                if (strcmp(direntIterator->d_name, ".") == 0) continue;
+                if (strcmp(direntIterator->d_name, "..") == 0) continue;
+                vElementStrings.push_back(path + "/" + direntIterator->d_name);
+            }
+            else break;
+        }
+        closedir (dirCurrentDirectory);
+    }
+    return vElementStrings;
+}
+
+bool PathIsDirectory(std::string path)
+{
+    DWORD dwFileAttributes = GetFileAttributesA(path.c_str());
+
+    if ((dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0)
+        return true;
+
+    else return false;
+}
+
+std::string FilenameFromPath(std::string path)
+{
+    size_t IndexLastSlash = path.find_last_of("/\\");
+    if (IndexLastSlash != std::string::npos)
+    {
+        return path.substr(IndexLastSlash + 1);
+    }
+    return path;
+}
+
+uint64_t GetFileSize(std::string path)
+{
+    WIN32_FIND_DATAA FileData;
+    HANDLE FileHandle = FindFirstFileA(path.c_str(), &FileData);
+    if (FileHandle == INVALID_HANDLE_VALUE)
+        return -1;
+
+    FindClose(FileHandle);
+    return (FileData.nFileSizeLow | (__int64)FileData.nFileSizeHigh << 32);
+}
+
+void PopulateTree(DiskElement &tree, std::string path)
+{
+    tree.name = path;
+    if (PathIsDirectory(path))
+    {
+        tree.is_directory = true;
+        tree.size = 0;
+        for (std::string DirElement : ListElementsInDirectory(path))
+        {
+            DiskElement child;
+            PopulateTree(child, DirElement);
+            tree.children.push_back(child);
+        }
+    }
+    else
+    {
+        tree.is_directory = false;
+        tree.size = GetFileSize(path);
+    }
 }
--- disk_crawler.h
+++ disk_crawler.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <numeric>
 #include <filesystem>
@@ -8,6 +9,20 @@
 #include <handleapi.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdint.h>
+
+struct DiskElement
+{
+    std::string name;
+    bool is_directory;
+    uint64_t size;
+    std::vector<DiskElement> children;
+};
 
 std::vector<std::string> ListDrives();
 std::vector<std::string> ListSubDirectories(std::string path);
+std::vector<std::string> ListElementsInDirectory(std::string path);
+bool PathIsDirectory(std::string path);
+std::string FilenameFromPath(std::string path);
+uint64_t GetFileSize(std::string path);
+void PopulateTree(DiskElement &tree, std::string path);
