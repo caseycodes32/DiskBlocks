0daf823d89432bf7b2595157fa37ec0fc06e1129
Kyle Meyer <vegas.kylem@gmail.com>
2025-03-24 02:39:43 -0700

    Added an animated loading symbol to  ui_helper
    - Added two functions that will return a symbol for implementing a loading animation, alternating every 200ms

--- ui_helper.cpp
+++ ui_helper.cpp
@@ -30,3 +30,28 @@ std::string GetDirectoryNameFromPath(std::string path)
 {
     return path.substr(path.find_last_of("/\\") + 1);
 }
+
+std::string GetCurrentLoadingSymbol()
+{
+    static long long start_time = curtime();
+    long long delta_time = curtime();
+
+    const char display_symbols[4] = {'|', '/', '-', '\\'};
+    static int current_symbol = 0;
+
+    if (delta_time - start_time > 200)
+    {
+        start_time = curtime();
+        if (current_symbol < 3) current_symbol++;
+        else current_symbol = 0;
+    }
+
+    return std::string(1, display_symbols[current_symbol]);
+}
+
+long long curtime()
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+      ).count();
+}
--- ui_helper.h
+++ ui_helper.h
@@ -4,7 +4,10 @@
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_win32.h"
 #include "disk_crawler.h"
+#include <chrono>
 
 void UIDirectoryTree(DiskElement &tree, std::string &selected_path);
 void PopulateSubDirectories(DiskElement &tree, std::string path);
 std::string GetDirectoryNameFromPath(std::string path);
+std::string GetCurrentLoadingSymbol();
+long long curtime();
